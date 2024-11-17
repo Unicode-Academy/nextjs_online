dedeef55d78f2ac736df8907b278ac8da56cc051
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-11-17 13:45:11 +0700

    Next swr 02

--- nextjs_swr/src/app/page.tsx
+++ nextjs_swr/src/app/page.tsx
@@ -1,27 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
+import useSWR from "swr";
+const getPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
+};
 export default function HomePage() {
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-  useEffect(() => {
-    const getPosts = async () => {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode1.com/posts"
-        );
-        const posts = await response.json();
-        setPosts(posts);
-      } catch (error) {
-        setError(error as Error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getPosts();
-  }, []);
+  const {
+    data: posts,
+    isLoading,
+    error,
+  } = useSWR("/posts", getPosts, {
+    fallbackData: [],
+  });
+
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
