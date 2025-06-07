14fd6c27ef17ae5533022c58518114d525a82734
Hoang An Unicode <hoangan.web@gmail.com>
2025-06-07 11:56:09 +0700

    Update code

--- nextjs_i18n/src/i18n/request.ts
+++ nextjs_i18n/src/i18n/request.ts
@@ -1,16 +1,23 @@
 import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { routing } from "./routing";
-
+const getTranslations = async (locale: string) => {
+  const response = await fetch(
+    `http://localhost:3000/translations/${locale}.json`
+  );
+  return response.json();
+};
 export default getRequestConfig(async ({ requestLocale }) => {
   // Typically corresponds to the `[locale]` segment
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
+  const translations = await getTranslations(locale);
 
   return {
     locale,
-    messages: (await import(`../translations/${locale}.json`)).default,
+    // messages: (await import(`../translations/${locale}.json`)).default,
+    messages: translations,
   };
 });
