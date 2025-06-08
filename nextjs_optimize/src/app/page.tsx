507bf2c847033a6e45c20768eeefdbf9cb88bd90
Hoang An Unicode <hoangan.web@gmail.com>
2025-06-08 18:15:05 +0700

    Toi uu Font chu

--- nextjs_optimize/src/app/page.tsx
+++ nextjs_optimize/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { openSans } from "@/utils/fonts";
+import clsx from "clsx";
 import Image from "next/image";
 // import img01 from "../images/img01.jpg";
 export default function HomePage() {
   return (
     <div className="w-[1200px] mx-auto">
       <h1 className="text-3xl font-bold underline">Tối ưu hình ảnh</h1>
+      <h2 className={clsx("text-2xl font-bold", openSans.className)}>
+        Ok chưa?
+      </h2>
       {/* <Image src={img01} width={300} alt="Hình ảnh 01" />
       <Image src={"/img01.jpg"} width={300} height={200} alt="Hình ảnh 02" /> */}
       <div className="flex gap-3">
---
+++ nextjs_optimize/src/utils/fonts.ts
@@ -0,0 +1,9 @@
+import { Roboto, Open_Sans } from "next/font/google";
+export const roboto = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+});
+export const openSans = Open_Sans({
+  weight: ["300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
