353a616fa89ffb478c33df9aafccb1feb7d04b8d
Hoang An Unicode <hoangan.web@gmail.com>
2025-06-08 16:29:32 +0700

    Toi uu hinh anh - Phan 2

--- nextjs_optimize/next.config.ts
+++ nextjs_optimize/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdnphoto.dantri.com.vn",
+      },
+      {
+        hostname: "static-images.vnncdn.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- nextjs_optimize/src/app/page.tsx
+++ nextjs_optimize/src/app/page.tsx
@@ -1,11 +1,49 @@
 import Image from "next/image";
-import img01 from "../images/img01.jpg";
+// import img01 from "../images/img01.jpg";
 export default function HomePage() {
   return (
     <div className="w-[1200px] mx-auto">
       <h1 className="text-3xl font-bold underline">Tối ưu hình ảnh</h1>
-      <Image src={img01} width={300} alt="Hình ảnh 01" />
-      <Image src={"/img01.jpg"} width={300} height={200} alt="Hình ảnh 02" />
+      {/* <Image src={img01} width={300} alt="Hình ảnh 01" />
+      <Image src={"/img01.jpg"} width={300} height={200} alt="Hình ảnh 02" /> */}
+      <div className="flex gap-3">
+        <div className="w-1/3 relative h-[200px]">
+          <Image
+            src={
+              "https://cdnphoto.dantri.com.vn/whz4r-gBTKJvVadE7KgWv_T_Lr8=/zoom/1032_688/2025/06/07/thi-lop-10-thanh-dong28jpg-1749306715454.jpg"
+            }
+            fill
+            alt="Hình ảnh 03"
+          />
+        </div>
+        <div className="w-1/3 relative h-[200px]">
+          <Image
+            src={
+              "https://cdnphoto.dantri.com.vn/whz4r-gBTKJvVadE7KgWv_T_Lr8=/zoom/1032_688/2025/06/07/thi-lop-10-thanh-dong28jpg-1749306715454.jpg"
+            }
+            fill
+            alt="Hình ảnh 03"
+          />
+        </div>
+        <div className="w-1/3 relative h-[200px]">
+          <Image
+            src={
+              "https://cdnphoto.dantri.com.vn/whz4r-gBTKJvVadE7KgWv_T_Lr8=/zoom/1032_688/2025/06/07/thi-lop-10-thanh-dong28jpg-1749306715454.jpg"
+            }
+            fill
+            alt="Hình ảnh 03"
+          />
+        </div>
+        <div className="w-1/3 relative h-[200px]">
+          <Image
+            src={
+              "https://static-images.vnncdn.net/vps_images_publish/000001/000003/2025/6/8/them-dai-gia-muon-lam-duong-sat-toc-do-cao-100-ty-usd-mien-phi-ve-6-thang-va-khong-can-dac-quyen-48129.jpg?width=760&s=k_Mynl248fsD3mlt7E4pdw"
+            }
+            fill
+            alt="Hình ảnh 03"
+          />
+        </div>
+      </div>
     </div>
   );
 }
