be50b892beb0252e01fb4ed038f286c5deb3bae2
Hoang An Unicode <hoangan.web@gmail.com>
2025-06-24 17:48:52 +0700

    Message Websocket

--- nextjs_socket/socket-server/app.js
+++ nextjs_socket/socket-server/app.js
@@ -2,11 +2,31 @@ import { WebSocketServer } from "ws";
 const server = new WebSocketServer({
   port: 8080,
 });
+const messageArr = [];
 server.on("connection", (socket) => {
   console.log("Client connected");
   socket.on("message", (message) => {
-    console.log(`Received: ${message}`);
-    socket.send(`Server: ${message}`);
+    const data = JSON.parse(message);
+    if (data.type === "send-message") {
+      messageArr.push(data.payload);
+      server.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            type: "send-message",
+            payload: messageArr,
+          })
+        );
+      });
+    }
+
+    if (data.type === "load-message") {
+      socket.send(
+        JSON.stringify({
+          type: "send-message",
+          payload: messageArr,
+        })
+      );
+    }
   });
 
   socket.on("close", () => {
--- nextjs_socket/src/app/_components/DemoSocket.tsx
+++ nextjs_socket/src/app/_components/DemoSocket.tsx
@@ -1,17 +1,63 @@
 "use client";
 
+import { useEffect, useState } from "react";
+const SOCKET_URL = "ws://localhost:8080";
 export default function DemoSocket() {
-  const ws = new WebSocket("ws://localhost:8080");
-  ws.onopen = () => {
-    console.log("Connected to server");
-    ws.send("Hello from client");
+  const [socket, setSocket] = useState<WebSocket>();
+  const [value, setValue] = useState<string>("");
+  const [messages, setMessages] = useState([]);
+  const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
   };
-  ws.onmessage = (event) => {
-    console.log(event.data);
-  };
-  ws.onclose = () => {
-    console.log("Disconnected from server");
+  useEffect(() => {
+    const ws = new WebSocket(SOCKET_URL);
+    setSocket(ws);
+
+    ws.addEventListener("message", (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === "send-message") {
+        setMessages(data.payload);
+      }
+    });
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "load-message",
+        })
+      );
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const handleClick = () => {
+    if (!socket) {
+      return;
+    }
+    const data = {
+      type: "send-message",
+      payload: value,
+    };
+    socket.send(JSON.stringify(data));
+    setValue("");
   };
 
-  return <div>DemoSocket</div>;
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Vui lòng nhập..."
+        onChange={handleChangeValue}
+        value={value}
+      />
+      <button onClick={handleClick}>Click me</button>
+      <div>
+        {messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
