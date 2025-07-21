c4dd66575aecee14e0d40929bd0e90f7e3439d08
Hoang An <contact@unicode.vn>
2025-07-21 11:04:17 +0700

    Notion Clone - Landing Page - P2

--- nextjs_notion_clone/src/app/(main-layout)/(home)/page.tsx
+++ nextjs_notion_clone/src/app/(main-layout)/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center min-h-full px-3">
@@ -15,6 +16,24 @@ export default function HomePage() {
         <Button size={null} className="rounded-[5px] py-3 px-5">
           Enter Jotion <ArrowRight />
         </Button>
+      </div>
+      <div className="max-w-5xl mx-auto flex items-center">
+        <div className="relative h-[400px] w-[300px] sm:w-[350px] md:w-[400px]">
+          <Image
+            src={`/documents.png`}
+            alt="Documents"
+            className="object-contain max-w-full"
+            fill
+          />
+        </div>
+        <div className="relative h-[400px] w-[300px] sm:w-[350px] md:w-[400px] hidden sm:block">
+          <Image
+            src={`/reading.png`}
+            alt="Reading"
+            className="object-contain max-w-full"
+            fill
+          />
+        </div>
       </div>
     </div>
   );
