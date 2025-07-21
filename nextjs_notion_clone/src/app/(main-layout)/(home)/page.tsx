1733982b031f034f697478a8176d4bef3f5ad10a
Hoang An <contact@unicode.vn>
2025-07-21 10:06:26 +0700

    Notion Clone - Landing Page - P1

--- nextjs_notion_clone/src/app/(main-layout)/(home)/page.tsx
+++ nextjs_notion_clone/src/app/(main-layout)/(home)/page.tsx
@@ -1,15 +1,21 @@
 import { Button } from "@/components/ui/button";
-
+import { ArrowRight } from "lucide-react";
 export default function HomePage() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold">Notion Demo</h1>
-      <Button
-        variant={"default"}
-        className="bg-[blueviolet] px-10 my-3 hover:bg-[green] cursor-pointer"
-      >
-        Click me
-      </Button>
+    <div className="flex flex-col justify-center min-h-full px-3">
+      <div className="max-w-3xl mx-auto text-center">
+        <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
+          Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+          <span className="underline">Jotion</span>
+        </h1>
+        <p className="text-base sm:text-xl md:text-2xl my-3">
+          Jotion is the connected workspace where <br /> better, faster work
+          happens.
+        </p>
+        <Button size={null} className="rounded-[5px] py-3 px-5">
+          Enter Jotion <ArrowRight />
+        </Button>
+      </div>
     </div>
   );
 }
--- nextjs_notion_clone/src/app/(main-layout)/layout.tsx
+++ nextjs_notion_clone/src/app/(main-layout)/layout.tsx
@@ -3,5 +3,5 @@ export default function MainLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="main-layout">{children}</div>;
+  return <div className="h-full">{children}</div>;
 }
