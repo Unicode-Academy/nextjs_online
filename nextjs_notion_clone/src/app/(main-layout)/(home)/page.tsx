e26c09a864cb9f0dd8b3fe3b530a9a66b14f3d4b
Hoang An <contact@unicode.vn>
2025-07-21 18:46:15 +0700

    Notion Clone - Fixed Scroll Header

--- nextjs_notion_clone/src/app/(main-layout)/(home)/page.tsx
+++ nextjs_notion_clone/src/app/(main-layout)/(home)/page.tsx
@@ -2,7 +2,7 @@ import Heading from "./_components/Heading";
 import Hero from "./_components/Hero";
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center min-h-full px-3">
+    <div className="flex flex-col justify-center min-h-full px-3 pt-25">
       <Heading />
       <Hero />
     </div>
--- nextjs_notion_clone/src/app/(main-layout)/_components/Header.tsx
+++ nextjs_notion_clone/src/app/(main-layout)/_components/Header.tsx
@@ -1,9 +1,19 @@
+"use client";
+import { useScrollTop } from "@/app/hooks/useScrollTop";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
+import { cn } from "@/lib/utils";
 
 export default function Header() {
+  const scroll = useScrollTop();
   return (
-    <div className="py-3 px-6 sm:flex sm:justify-between">
+    <div
+      className={cn(
+        "py-3 px-6 sm:flex sm:justify-between fixed top-0 w-full z-50",
+        scroll &&
+          "border-b border-gray-200 bg-white dark:bg-[#000] dark:border-gray-800 shadow-xs"
+      )}
+    >
       <Logo />
       <NavBar />
     </div>
