da1dbed516203aff42ef800dbf0cc31a72a361aa
Hoang An <contact@unicode.vn>
2025-07-27 17:29:09 +0700

    Notion Clone - User Settings - Phan 2

--- nextjs_notion_clone/src/app/(documents)/_components/UserItem.tsx
+++ nextjs_notion_clone/src/app/(documents)/_components/UserItem.tsx
@@ -7,30 +7,46 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useUser } from "@clerk/nextjs";
+import { SignOutButton, useUser } from "@clerk/nextjs";
 import { AvatarImage } from "@radix-ui/react-avatar";
+import { ChevronsUpDown } from "lucide-react";
 export default function UserItem() {
   const { user } = useUser();
+  //   console.log(user);
 
   return (
     <DropdownMenu>
       <div className="cursor-pointer hover:bg-[#ddd] p-2">
-        <DropdownMenuTrigger className="flex gap-2 items-center cursor-pointer">
+        <DropdownMenuTrigger className="text-left flex gap-2 items-center cursor-pointer outline-0 block w-full">
           <Avatar>
             <AvatarImage src={user?.imageUrl} />
           </Avatar>
           <span className="line-clamp-1 me-3 text-left text-sm font-medium">
             {user?.fullName}
           </span>
+          <ChevronsUpDown size={15} className="text-muted-foreground" />
         </DropdownMenuTrigger>
       </div>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuContent align="start">
+        <DropdownMenuLabel className="w-[300px]">
+          <p className="text-[13px] text-muted-foreground mb-3">
+            {user?.emailAddresses[0].emailAddress}
+          </p>
+          <div className="flex gap-2 items-center">
+            <Avatar>
+              <AvatarImage src={user?.imageUrl} />
+            </Avatar>
+            <span className="line-clamp-1 me-3 text-left">
+              {user?.fullName}
+            </span>
+          </div>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+        <DropdownMenuItem className="focus:bg-transparent">
+          <SignOutButton>
+            <span className="cursor-pointer text-muted-foreground">Logout</span>
+          </SignOutButton>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
