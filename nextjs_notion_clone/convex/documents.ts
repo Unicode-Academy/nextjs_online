057a64358dd3e7ebf433de0df0fa9959e783f97d
Hoang An <hoangan@fullstack.edu.vn>
2025-08-16 17:38:53 +0700

    Notion Clone - Trashbox - 02

--- nextjs_notion_clone/convex/documents.ts
+++ nextjs_notion_clone/convex/documents.ts
@@ -89,3 +89,72 @@ export const getSidebar = query({
     return documents;
   },
 });
+
+export const getTrash = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("byUserId", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("isArchived"), true))
+      .order("desc")
+      .collect();
+    return documents;
+  },
+});
+
+export const restore = mutation({
+  args: {
+    id: v.id("documents"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      isArchived: false,
+    });
+
+    return document;
+  },
+});
+
+export const remove = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.delete(args.id);
+    return document;
+  },
+});
--- nextjs_notion_clone/src/app/(documents)/_components/Trashbox.tsx
+++ nextjs_notion_clone/src/app/(documents)/_components/Trashbox.tsx
@@ -1,5 +1,36 @@
-import { Search } from "lucide-react";
+import { useMutation, useQuery } from "convex/react";
+import { Undo, Trash, Search } from "lucide-react";
+import { api } from "../../../../convex/_generated/api";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { toast } from "sonner";
 export default function Trashbox() {
+  const documents = useQuery(api.documents.getTrash);
+  const restore = useMutation(api.documents.restore);
+  const remove = useMutation(api.documents.remove);
+  const handleRestore = (id: Id<"documents">) => {
+    const promise = restore({ id });
+    toast.promise(promise, {
+      loading: "Restoring note...",
+      success: "Note restored successfully",
+      error: "Failed to restore note",
+    });
+  };
+  const handleRemove = (id: Id<"documents">) => {
+    const promise = remove({ id });
+    toast.promise(promise, {
+      loading: "Removing note...",
+      success: "Note removed successfully",
+      error: "Failed to remove note",
+    });
+  };
+  if (!documents) {
+    return (
+      <div className="h-full flex items-center justify-center p-4">
+        <LoadingSpinner />
+      </div>
+    );
+  }
   return (
     <div className="text-sm">
       <div className="flex gap-1 items-center p-2">
@@ -11,9 +42,37 @@ export default function Trashbox() {
         />
       </div>
       <div className="mt-2 px-1 pb-1">
-        <p className="text-xs text-center text-muted-foreground pb-2">
+        <p className="hidden text-xs text-center text-muted-foreground pb-2 last:block">
           No documents found.
         </p>
+        {documents?.map((document) => (
+          <div
+            key={document._id}
+            className="text-sm rounded-sm w-full flex items-center text-primary justify-between hover:bg-primary/5 cursor-pointer"
+          >
+            <span className="truncate pl-2">{document.title}</span>
+            <div className="flex items-center">
+              <div
+                onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+                  e.stopPropagation();
+                  handleRestore(document._id);
+                }}
+                className="rounded-sm p-2 hover:bg-neutral-200 cursor-pointer dark:hover:bg-neutral-600"
+              >
+                <Undo className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <div
+                onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+                  e.stopPropagation();
+                  handleRemove(document._id);
+                }}
+                className="rounded-sm p-2 hover:bg-neutral-200 cursor-pointer"
+              >
+                <Trash className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
